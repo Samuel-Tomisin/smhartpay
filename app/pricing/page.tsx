240790356eca1f3f27d94ac29f4b5327126486ac
Samuel-Tomisin <samueltomisin01@gmail.com>
2026-07-08 15:07:23 +0100

    update smhartpay project

---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+
+export default function PricingPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Pricing Page</h1>
+      <p className="text-lg text-gray-600">
+        Welcome to the pricing page of our application.
+      </p>
+    </div>
+  );
+}
